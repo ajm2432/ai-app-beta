46108b14d3a294c66d702429f37b58c720268149
Austin Miller <ausmillr@amazon.com>
2023-02-04 01:47:38 -0500

    add admin and profile pages

--- copy_secrets.sh
+++ copy_secrets.sh
@@ -10,8 +10,8 @@ SECRET_NAME="AWS_ACCESS_KEYS"
 KEY="AWS_ACCESS_KEY"
 SECRET_KEY="AWS_SECRET_ACCESS_KEY"
 # Use the AWS CLI to retrieve the secret value
-AWS_ACCESS_KEY=$(aws secretsmanager get-secret-value --profile personal --region $REGION --secret-id $SECRET_NAME --query 'SecretString' --output text | jq -r .$KEY)
-AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value --profile personal --region $REGION --secret-id $SECRET_NAME --query 'SecretString' --output text | jq -r .$SECRET_KEY)
+AWS_ACCESS_KEY=$(aws secretsmanager get-secret-value  --region $REGION --secret-id $SECRET_NAME --query 'SecretString' --output text | jq -r .$KEY)
+AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value  --region $REGION --secret-id $SECRET_NAME --query 'SecretString' --output text | jq -r .$SECRET_KEY)
 # Assign the secret value to an environment variable
 export AWS_ACCESS_KEY=$AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
