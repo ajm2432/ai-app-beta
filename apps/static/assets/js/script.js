3a82d090eff1cb5fa5c7f88cd0680ee2a21033d9
Austin Miller <ausmillr@amazon.com>
2023-02-06 02:54:49 -0500

    add typing dots

--- apps/static/assets/js/script.js
+++ apps/static/assets/js/script.js
@@ -5,7 +5,7 @@ const conversation = document.getElementById("conversation");
 const username = document.getElementById("username").textContent;
 // Add an event listener to the send button to handle sending messages
 sendButton.addEventListener("click", sendMessage);
-
+document.addEventListener("keydown", function(event) { if (event.keyCode == 13) { sendButton.click(); } });
 // Send the user's message to the OpenAI API and display the bot's response
 function sendMessage() {
   // Get the user's message from the input field
@@ -17,6 +17,19 @@ function sendMessage() {
   conversation.appendChild(userMessage);
   // Send the message to the OpenAI API
   if(window.location.pathname == '/chat'){
+    let dots = 0;
+    function addDot() {
+      let dot = ".";
+      dots++;
+      if (dots > 3) {
+        dots = 0;
+      }
+      for (let i = 0; i < dots; i++) {
+        dot += ".";
+      }
+      console.log(dot);
+    }
+    setInterval(addDot, 500);
   fetch("/send-message", {
     method: "POST",
     body: JSON.stringify({ message }),
@@ -32,6 +45,25 @@ function sendMessage() {
     });
   }
   if(window.location.pathname == '/emoji-chat'){
+    let dots = 0;
+    const botMessage = document.createElement("div");
+    conversation.appendChild(botMessage);
+    
+    function addDot() {
+      let dot = ".";
+      dots++;
+      if (dots > 3) {
+        dots = 0;
+      }
+      for (let i = 0; i < dots; i++) {
+        dot += ".";
+      }
+      botMessage.innerHTML = "Austin A.I" + dot;
+      botMessage.className = "container bg-white rounded p-2 text-dark float-end"
+    }
+    
+    let intervalId = setInterval(addDot, 500);
+    
     fetch("/send-message-emoji", {
       method: "POST",
       body: JSON.stringify({ message }),
@@ -39,8 +71,9 @@ function sendMessage() {
     })
       .then((response) => response.json())
       .then((data) => {
+      clearInterval(intervalId);
+      botMessage.innerHTML = "";
         // Display the bot's response in the conversation area
-        const botMessage = document.createElement("div");
         botMessage.innerHTML = "Austin A.I: " + data.response;
         botMessage.className = "container bg-white rounded p-2 text-dark float-end"
         conversation.appendChild(botMessage);
